1d98a992fdae655a792c0fc9a7d7e84c846e4de2
UbunTom <thomas.collingwood@gmail.com>
2013-05-05 17:07:25 +0200

    Update autofarm.js

--- autofarm.js
+++ autofarm.js
@@ -1,13 +1,16 @@
-$.getScript("https://raw.github.com/UbunTom/TWFarmer/master/attackSender.js",function(){});
-
-$.getScript("http://gamemash.co.uk/villagelist.php?id="+vill+get,function(){
-for(var v=0;v<villages.length;v++)
-{
-	if(ok==false)
+$.getScript("https://raw.github.com/UbunTom/TWFarmer/master/attackSender.js",function(){	
+	
+	$.getScript("http://gamemash.co.uk/villagelist.php?id="+vill+get,function(){
+	for(var v=0;v<villages.length;v++)
 	{
-		break;
+		if(ok==false)
+		{
+			break;
+		}
+		send(v);
 	}
-	send(v);
-}
-//setTimeout(document.write("Finished sending to: "+villages.length+" villages<br />"),1000);
+	});	
+	
 });
+
+
