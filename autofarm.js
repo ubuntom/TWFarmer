ea473c0654a7968778064c4d98122bb64ced2b1d
UbunTom <thomas.collingwood@gmail.com>
2013-06-05 17:28:34 +0200

    Update autofarm.js

--- autofarm.js
+++ autofarm.js
@@ -1,3 +1,5 @@
+//Note: use of this script may get you banned
+
 $.getScript("https://raw.github.com/UbunTom/TWFarmer/master/attackSender.js",function(){	
 	
 	$.getScript("http://gamemash.co.uk/villagelist.php?id="+vill+get,function(){
